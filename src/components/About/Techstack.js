9e3610c06bf382a6612af94b49686a34be8bec2f
Sanji910 <jordanaraujo313@gmail.com>
2024-07-06 00:22:42 -0700

    portfolio

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+  DiPostgresql,
+  DiUnitySmall,
+  DiWordpress,
+  DiSwift,
+  DiLinux,
+  DiLaravel,
+  DiGithub,
+  DiHtml5,
+  DiCss3,
+  DiJavascript,
+  DiDjango,
+  DiAndroid,
+  DiCompass,
+  DiJira,
+} from "react-icons/di";
+import {
+  SiRedis,
+  SiFirebase,
+  SiNextdotjs,
+  SiSolidity,
+  SiPostgresql,
+} from "react-icons/si";
+import { TbBrandGolang } from "react-icons/tb";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={1} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <TbBrandGolang />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <SiSolidity />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <SiNextdotjs />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <SiRedis />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiPostgresql/>
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiUnitySmall />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiWordpress />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiSwift />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiLinux />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiLaravel />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiGithub />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiJira />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiDjango />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiAndroid />
+      </Col>
+      <Col xs={4} md={1} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            Hello, I'm a versatile
+            <b className="purple"> Full stack developer</b> with expertise in both <b className="purple">Frontend</b> and <b className="purple">Backend</b> development.
+              <br />
+              <br />I specialize in
+              <i>
+                <b className="purple"> software engineering </b>and<b className="purple"> blockchain technology </b>, delivering robust and scalable solutions.
+              </i>
+              <br />
+              <br />
+              <i>
+              My passion lies in creating seamless user experiences and secure, innovative systems.
+              </i>
+              <br />
+              <br />
+              <i>
+                Let's connect and bring your vision to Life!
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>  
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https:/gmail.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineMail />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/jordan-araujo-80b687314/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
